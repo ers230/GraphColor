b999f630dd92c2c2895c4fc60f0237897efff148
ers230 <edwinrotgans@gmail.com>
2017-08-02 15:10:58 +0200

    Git in VS code

--- colorNodes.fsx
+++ colorNodes.fsx
@@ -1,17 +1,18 @@
 #load "./GraphColor.fs"
 
-
 open System
 open EdwinRotgans.ColorGraph
 
-
+// Test data
 let vertices = [("a","b");("a","e");("b","c");("b","f");("c","a");("c","f");("d","e");("e","a")]  
 
+// Assign colors to nodes
 let coloredNodes = colorGraphDummyPalet vertices 
 
-// Output
+// Output to screen
 Console.WriteLine "\n(Node, color)"
 coloredNodes |> List.iter (sprintf "%A" >> Console.WriteLine)
 Console.WriteLine "\nPress Enter to quit\n"
 Console.ReadLine() |> ignore
 
+let test = true
